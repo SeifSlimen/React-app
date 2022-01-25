fd312ad3cad12c5cc5beea6ff820641da15898d5
Seif Slimen <SeifSlimen38@gmail.com>
2022-01-25 15:33:44 +0100

    feat: Add Card Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Card from "./Components/Card";
 
 export default function App() {
   return (
     <div className="container">
       <Navbar />
       <Hero />
+      <Card /> 
+      <Card /> 
+      <Card /> 
+      <Card /> 
+
     </div>
   );
 }
